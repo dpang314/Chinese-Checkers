4460953fe10cc8861058a5b06dc322dcdc5d8c00
David Pang <davidpang314@gmail.com>
2022-01-19 14:08:06 -0500

    Set up Maven

---
+++ README.md
@@ -0,0 +1,1 @@
+# AOOD Group Project
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+    @Test
+    @DisplayName("Add two integers")
+    void add() {
+        assertEquals(3, Hello.add(1, 2));
+    }
+}
