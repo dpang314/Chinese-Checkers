edaad4da4dfebb7f99cafaa5841ddac010626442
David Pang <davidpang314@gmail.com>
2022-02-15 09:25:59 -0500

    additions to game, added type to player and owner to move

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -1,11 +1,17 @@
 
 public class Move {
+  private Player owner;
 	private Position startPosition;
 	private Position endPosition;
 	
-	public Move(Position startPosition, Position endPosition) {
+	public Move(Position startPosition, Position endPosition, Player owner1) {
 		this.startPosition = startPosition;
 		this.endPosition = endPosition;
+		this.owner = owner1;
+	}
+
+	public Player getOwner(){
+	    return owner;
 	}
 	
 	public Position getStartPosition() {
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,9 +3,14 @@ import java.awt.Color;
 public abstract class Player {	
 	private Color color;
 	private String playerName;
-	
+
+
 	public Player(Color color, String playerName) {};
 	public abstract Move getMove(Board board);
 	public Color getColor() { return color; };
 	public String getName() { return playerName; };
+  public String type(){
+    return "human";
+  }
+
 }
