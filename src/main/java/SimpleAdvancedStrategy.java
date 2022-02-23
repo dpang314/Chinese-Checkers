9cddd0533c58cbb5e91b02642f029eb79fefc38e
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-02-23 12:37:27 -0500

    "getPoint" method added and implemented

--- src/main/java/SimpleAdvancedStrategy.java
+++ src/main/java/SimpleAdvancedStrategy.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.geom.*;
 
 public class SimpleAdvancedStrategy extends Player {
 
@@ -8,8 +9,18 @@ public class SimpleAdvancedStrategy extends Player {
 
 	@Override
 	public Move getMove(Board board) {
-
 		return null;
 	}
-
+	
+	private static Point2D.Double getPoint(Position p) {
+		final int centerRow = 8;
+		
+		//distance between row and middle-row, multiplied by cos30 to get vertical distance
+		double y = (centerRow-p.getRow()) * (Math.cos(Math.PI/6));
+		
+		//distance from center of row to index
+		double x = p.getColumn()-((double)(Board.rowWidths[p.getRow()]-1)/2);
+		
+		return new Point2D.Double(x,y);
+	}
 }
