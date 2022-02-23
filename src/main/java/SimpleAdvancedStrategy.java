84a6dece5b7aa8f7f452724748f85ff6970f5f8f
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-02-23 08:39:47 -0500

    added class blueprint

---
+++ src/main/java/SimpleAdvancedStrategy.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public class SimpleAdvancedStrategy extends Player {
+
+	public SimpleAdvancedStrategy(Color color, String playerName) {
+		super(color, playerName);
+	}
+
+	@Override
+	public Move getMove(Board board) {
+
+		return null;
+	}
+
+}
