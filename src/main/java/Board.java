7c0cf48964f2ae576d724a66f23e990c37a5e2f5
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-03-04 09:05:39 -0500

    Merge branch 'Board' of https://github.com/dpang314/AOOD-Group-Project.git into Board
    also fixed board and strategy initializations

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -65,10 +65,16 @@ public class Board implements Cloneable {
 		new Position(14, 0), new Position(14, 1), new Position(14, 2),
 		new Position(13, 0), new Position(13, 1), new Position(13, 2), new Position(13, 3)
 	};
-	public static final ArrayList<Position[]> homeAll = new ArrayList<Position[]>();
+	
+	public static final ArrayList<Position[]> homeAll = makeHomeAll();
+	
+	private static ArrayList<Position[]> makeHomeAll() {
+		ArrayList<Position[]> homeAll = new ArrayList<Position[]>();
+		Collections.addAll(homeAll, homeR, homeBu, homeBk, homeW, homeG, homeY);
+		return homeAll;
+	}
 	
 	public Board(Player[] players) {
-		Collections.addAll(homeAll, homeR, homeBu, homeBk, homeW, homeG, homeY);
 		for (int i=0; i<players.length; i++) {
 			if (!(players[i] == null)) {
 				populateReg(getHomeRegion(players[i].getColor()), players[i]);
@@ -136,7 +142,7 @@ public class Board implements Cloneable {
 		return ret;
 		
 	}
-	public Position[] getWinRegion(int i) {
+	public static Position[] getWinRegion(int i) {
 		return homeAll.get(i);
 	}
 	public ArrayList<Position> possibleMoves(Position p, boolean jumpOnly) {  
