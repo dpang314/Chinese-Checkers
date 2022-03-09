2e4bf69f721ea86e84be0bf43158fe75cbae95fb
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-03-09 17:45:34 -0500

    crappygame is good and can return total moves to complete a game

--- src/main/java/CrappyGame.java
+++ src/main/java/CrappyGame.java
@@ -1,84 +1,98 @@
 import java.awt.Color;
-
 public class CrappyGame {
+	private static QuinnStrategy blue, white, yellow, red, black, green;
+	private static Board board;
+	private static Player[] players;
+	private static int msDelay = 0;
 	
-	static QuinnStrategy blue = new QuinnStrategy(Color.BLUE, "BLUE"), white = new QuinnStrategy(Color.WHITE, "WHITE"), yellow = new QuinnStrategy(Color.YELLOW, "YELLOW"), red = new QuinnStrategy(Color.RED, "RED"), black = new QuinnStrategy(Color.BLACK, "BLACK"), green = new QuinnStrategy(Color.GREEN, "GREEN");
+	private static void setupGame() {
+		
+		blue = new QuinnStrategy(Color.BLUE, "BLUE");
+		white = new QuinnStrategy(Color.WHITE, "WHITE");
+		yellow = new QuinnStrategy(Color.YELLOW, "YELLOW");
+		red = new QuinnStrategy(Color.RED, "RED");
+		black = new QuinnStrategy(Color.BLACK, "BLACK");
+		green = new QuinnStrategy(Color.GREEN, "GREEN");
+		
+		/*
+		 * PLEASE DON'T CHANGE ANYTHING
+		 * BESIDES THIS BLOCK OF CODE
+		 * UNLESS YOU KNOW WHAT YOU'RE DOING
+		 */
+		{
+		msDelay = 0; //the delay between each move, in ms
+		players = new Player[] {blue};
+		}
+		
+		
+		board = new Board(players);
+		
+	}
 	
-	/*
-	 * PLEASE DON'T CHANGE ANYTHING
-	 * BESIDES THIS BLOCK OF CODE
-	 * UNLESS YOU KNOW WHAT YOU'RE DOING
-	 */
+	//performs a move for a single player
+	private static void doMove(Player p, boolean verbose) {
+		Move playerMove;
+		
+		//gets and executes moves as long as the return value is not null
+		do {
+			
+			if(p.getWR()==null) break;
+			playerMove = p.getMove(board);
+			board.move(playerMove);
+			if(verbose) board.printBoard();
+			
+		} while (playerMove!=null);
+		
+		try {
+			Thread.sleep(msDelay);
+		} catch (InterruptedException e) {
+			System.err.println(e.getMessage());
+		}
+		
+	}
 	
-	//****************************\\
+	private static Player gameEnd(Board b) {
+		for(Player player: players) {
+			
+			boolean filledByOpp = false;
+			boolean unfilledPosition = false;
+			
+			for(Position p : Board.getWinRegion(player.getColor())) {
+				Peg peg = b.getPegs()[p.getRow()][p.getColumn()];
+				if(peg==null) {
+					unfilledPosition = true;
+					break;
+				}
+				if(peg.getOwner().getColor().equals(player.getColor())) {
+					filledByOpp = true;
+				}
+			}
+			
+			if(!unfilledPosition && filledByOpp)
+			return player;
+		}
+		return null;
+	}
 	
-	static int msDelay = 500;
-	
-	static Player[] players = new Player[]
-		{blue,red};
-	
-	//****************************//
+	public static int doGame(boolean verbose) {
+		setupGame();
+
+		int turns=0;
+		Player winner = null;
+		
+		while(winner==null) {
+			for(Player player : players) {
+				doMove(player, verbose);
+				winner = gameEnd(board);
+				if(winner!=null) break;
+			}
+			turns++;
+		}
+		
+		return turns;
+	}
 	
 	public static void main(String[] args) {
-		
-		Board b = new Board(players);
-		
-		Move blueMove;
-		Move whiteMove;
-		Move yellowMove;
-		Move redMove;
-		Move blackMove;
-		Move greenMove;
-		
-		while(true) {
-		
-			do {
-				if(blue.getWR()==null) break;
-				blueMove = blue.getMove(b);
-				b.move(blueMove);
-				b.printBoard();
-			} while (blueMove!=null);
-			
-			do {
-				if(white.getWR()==null) break;
-				whiteMove = white.getMove(b);
-				b.move(whiteMove);
-				b.printBoard();
-			} while (whiteMove!=null);
-			
-			do {
-				if(yellow.getWR()==null) break;
-				yellowMove = yellow.getMove(b);
-				b.move(yellowMove);
-				b.printBoard();
-			} while (yellowMove!=null);
-			
-			do {
-				if(red.getWR()==null) break;
-				redMove = red.getMove(b);
-				b.move(redMove);
-				b.printBoard();
-			} while (redMove!=null);
-			
-			do {
-				if(black.getWR()==null) break;
-				blackMove = black.getMove(b);
-				b.move(blackMove);
-				b.printBoard();
-			} while (blackMove!=null);
-			
-			do {
-				if(green.getWR()==null) break;
-				greenMove = green.getMove(b);
-				b.move(greenMove);
-				b.printBoard();
-			} while (greenMove!=null);
-			
-			try {
-				Thread.sleep(msDelay);
-			} catch (InterruptedException e) {
-				System.err.println(e.getMessage());
-			}
-		}
+		doGame(true);
 	}
 }
